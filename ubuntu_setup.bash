14113e8a2727c7026a8dd4dc248e208c8c48034b
Darren P Meyer <darrenpmeyer@users.noreply.github.com>
2023-03-01 16:06:01 -0600

    small fixes to docs/comments - README.md linkify setup - ubuntu_setup.bash fix and improve comments

--- ubuntu_setup.bash
+++ ubuntu_setup.bash
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# paste this _entire contents_, including the #!, as your user data
+
 NON_ROOT_USER=${NON_ROOT_USER:-ubuntu}
 
+# preflight
 apt-get update
 apt-get upgrade -y
 apt-get install -y python3 python3-pip python3-venv curl
@@ -13,7 +16,7 @@ apt-get install -y maven gradle golang nodejs rustc cargo cargo-lock
 # Add the JDKs you'll need
 apt-get install -y openjdk-19-jdk
 
-# install -- replace PACKAGE_URL with your .tar.gz release URL
+#install module
 su ubuntu -c "cd \$HOME; python3 -m venv endor-venv; endor-venv/bin/pip3 install -e 'git+https://github.com/endorlabs/cloud-scanworker.git#egg=endor_scanworker&subdirectory=scanworker'"
 
 # run -- add any scanworker options required to the end of the line
